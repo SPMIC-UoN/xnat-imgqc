3c5704b35878f5cc1351ca03f834d578c4c441ef
Martin Craig <martin.craig@nottingham.ac.uk>
2023-01-20 10:35:19 +0000

    Better error handling and CSV output

--- docker/imgqc_popstats.py
+++ docker/imgqc_popstats.py
@@ -68,9 +68,9 @@ def get_project(options):
         r = requests.get(url, verify=False, cookies=options.cookies, auth=options.auth, params=params)
         if r.status_code == 200:
             break
-
     if r.status_code != 200:
         raise RuntimeError(f"Failed to download projects after 10 tries: {r.status_code} {r.text}")
+
     projects = list(csv.DictReader(io.StringIO(r.text)))
     for project in projects:
         if project["ID"] == options.project or project["name"] == options.project:
@@ -81,45 +81,44 @@ def get_project(options):
 
 def add_results_from_session(options, session):
     """
-    FIXME: raise_for_status
+    Get the results from the ImgQC assessor and add them to matching results list
+
+    This assumes that each result from the assessor matches exactly one row in the config.
+    The same assumption is made when the QC is run.
     """
     url = "%s/data/experiments/%s/assessors/" % (options.host, session["ID"])
     LOG.info(f"Getting imgqc assessors from URL: {url}")
     r = requests.get(url, auth=options.auth, cookies=options.cookies, params={"format" : "csv", "xsiType" : "xnat_imgqc:ImgQCData", "columns" : "ID"}, verify=False)
-    if r.status_code == 200:
-        f = StringIO(r.text)
-        scans = []
-        for row in csv.DictReader(f, skipinitialspace=True):
-            url = "%s/data/experiments/%s/assessors/%s" % (options.host, session["ID"], row['ID'])
-            LOG.info(f"Getting ImqQC results from URL: {url}")
-            r = requests.get(url, auth=options.auth, cookies=options.cookies, params={"format" : "xml"}, verify=False)
-            if r.status_code == 200:
-                d = xmltodict.parse(r.text)["xnat_imgqc:ImgQCData"]
-                scans = d.get("xnat_imgqc:scan", [])
-            else:
-                LOG.warn(f"Failed to get imgqc results for session {session['ID']} - skipping")
-                LOG.debug(r.status_code)
-                LOG.debug(r.text)
-                return
-    else:
-        LOG.warn(f"Failed to get imgqc results for session {session['ID']} - skipping")
-        LOG.debug(r.status_code)
-        LOG.debug(r.text)
-        return
+    r.raise_for_status()
+    f = StringIO(r.text)
+    scans = []
+    for row in csv.DictReader(f, skipinitialspace=True):
+        url = "%s/data/experiments/%s/assessors/%s" % (options.host, session["ID"], row['ID'])
+        LOG.info(f"Getting ImqQC results from URL: {url}")
+        r = requests.get(url, auth=options.auth, cookies=options.cookies, params={"format" : "xml"}, verify=False)
+        r.raise_for_status()
+        d = xmltodict.parse(r.text)["xnat_imgqc:ImgQCData"]
+        scans = d.get("xnat_imgqc:scan", [])
 
+    if isinstance(scans, dict):
+        scans = [scans]
     for scan in scans:
         tests = scan.get("xnat_imgqc:test", [])
         if isinstance(tests, dict):
             tests = [tests]
         for test in tests:
             img_name = test["xnat_imgqc:img"]
-            # For each test found, loop through test definition and find the relevant entries
-            # FIXME vendors is not being used
-            for test_def, test_results in zip(options.tests, options.test_results):
-                matchers, exclusions, vendors, test_name, mean, std, amber, red = test_def
-                if test["xnat_imgqc:name"] == test_name and test_is_relevant(img_name, matchers, exclusions, vendors):
-                    LOG.info(f"Adding {test_name} result {test['xnat_imgqc:result']} for {img_name} to matcher: {matchers}, exclusions {exclusions}")
-                    test_results.append(float(test["xnat_imgqc:result"]))
+            result = test["xnat_imgqc:result"]
+            try:
+                # For each test found, loop through test definition and find the relevant entries
+                # FIXME vendors is not being used
+                for test_def, test_results in zip(options.tests, options.test_results):
+                    matchers, exclusions, vendors, test_name, mean, std, amber, red = test_def
+                    if test["xnat_imgqc:name"] == test_name and test_is_relevant(img_name, matchers, exclusions, vendors):
+                        LOG.info(f"Adding {test_name} result {result} for {img_name} to matcher: {matchers}, exclusions {exclusions}")
+                        test_results.append(float(result))
+            except:
+                LOG.exception(f"Failed to process test result: {test} - ignoring")
 
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, **kwargs):
@@ -158,9 +157,24 @@ def main():
             LOG.info(f"Processing session {idx}: {session['label']}")
             add_results_from_session(options, session)
         
-        for test_def, test_results in zip(options.tests, options.test_results):
-            print(test_def, np.mean(test_results), np.std(test_results))
-
+        writer = None
+        with open("imgqc_popstats.csv", "w") as f:
+            for test_def, test_results in zip(options.tests, options.test_results):
+                matchers, exclusions, vendors, test_name, mean, std, amber, red = test_def
+                outdata = {
+                    "matchers" : ",".join(matchers),
+                    "exclusions" : ",".join(exclusions),
+                    "vendors" : ",".join(vendors),
+                    "test_name" : test_name,
+                    "mean" : np.mean(test_results),
+                    "std" : np.std(test_results),
+                    "amber" : amber,
+                    "red" : red,
+                }
+                if writer is None:
+                    writer = csv.DictWriter(f, outdata.keys())
+                    writer.writeheader()
+                writer.writerow(outdata)
     except Exception as exc:
         LOG.error(exc)
         traceback.print_exc()
